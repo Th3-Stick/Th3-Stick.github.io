9238a76005451aa9859063334e58d2cd7ffaae7f
Th3-Stick <delaigue.adrien@gmail.com>
2023-06-12 16:48:07 +0200

    Mise en place template + Dark Mode

---
+++ js/dark.js
@@ -0,0 +1,26 @@
+const theme = {
+
+    init: function () {
+
+        theme.switchButton = document.querySelector("#theme-switch");
+        theme.body = document.querySelector("body");
+        theme.header = document.querySelector("header");
+        theme.btn = document.querySelector(".bi");
+        theme.switchButton.addEventListener("click", theme.handleClickDark);
+    },
+
+    handleClickDark: function () {
+        theme.body.classList.toggle("theme-dark");
+        theme.header.classList.toggle("dark-font");
+
+        if (theme.btn.classList.contains("bi-moon-fill")) {
+            theme.btn.classList.remove("bi-moon-fill");
+            theme.btn.classList.add("bi-sun-fill");
+        } else if (theme.btn.classList.contains("bi-sun-fill")) {
+            theme.btn.classList.remove("bi-sun-fill");
+            theme.btn.classList.add("bi-moon-fill");
+        }
+    },
+}
+
+theme.init();
